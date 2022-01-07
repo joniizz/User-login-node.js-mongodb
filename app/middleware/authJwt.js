0c98d89784b7b07d43b029e6f9bf7370c5837025
joniizz <41981946+joniizz@users.noreply.github.com>
2022-01-08 00:24:09 +1100

    verify jwt

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -4,6 +4,24 @@ const db = require("../models/index");
 const User = db.user;
 
 verifyToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+    //403 Forbidden 
+    if(!token) {
+        return res.status(403).send({message: "No token provieded!" });
+    };
     
+    jwt.verify(token, config.secret, (err, decoded) => {
+        //401 Unauthorized
+        if(err) {
+            return res.status(401).send({message: "Unauthorzied"});
+        };
+        req.userId = decoded.id;
+        next();
+    });
+};
+
+const authJwt = {
+    verifyToken
 }
+module.exports = authJwt;
 
